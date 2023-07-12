59e8b8234e193cca5a1882273c1bfb74b8a220a7
DenysChebotarov <114431465+DenysChebotarov@users.noreply.github.com>
2023-07-12 12:49:32 +0300

    hw03-mongodb finally

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,22 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const {validateBody} = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
-const shemas = require("../../schemas/contacts")
+const {schemas} = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", ctrl.listContacts);
 
-router.get("/:id", ctrl.getContactById);
+router.get("/:id", isValidId, ctrl.getContactById);
 
-router.post("/", validateBody(shemas.addSchema), ctrl.addContact );
+router.post("/", validateBody(schemas.addSchema), ctrl.addContact );
 
-router.delete("/:id", ctrl.removeContact);
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
 
-router.put("/:id", validateBody(shemas.addSchema), ctrl.updateContact);
+router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite );
+
+router.delete("/:id", isValidId, ctrl.removeContact);
 
 module.exports = router;
