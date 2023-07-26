7693e4cefdb60c9fbf05ecf3fcfefafb73ae4053
DenysChebotarov <114431465+DenysChebotarov@users.noreply.github.com>
2023-07-26 11:51:03 +0300

    hw-06-email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
-const { User } = require("../models/user");
-const { HttpError, ctrlWrapper } = require("../helpers");
+const { User, schemas } = require("../models/user");
+const { HttpError, ctrlWrapper, sendEmail } = require("../helpers");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const gravatar = require('gravatar');
 const path = require("path");
 const fs = require("fs/promises");
+const {nanoid} = require("nanoid");
 const Jimp = require("jimp");
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, BASE_URL } = process.env;
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
@@ -18,12 +19,61 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
-  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+  const verificationToken = nanoid();
+
+
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationToken });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click verify email</a>`
+};
+await sendEmail(verifyEmail);
 
   res.status(201).json({
     email: newUser.email,
     name: newUser.name,
     subscription: newUser.subscription,
+  });
+};
+
+const verifyEmail = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    throw HttpError(404);
+  }
+  await User.findByIdAndUpdate(user._id, {
+    verificationToken: "",
+    verify: true,
+  });
+  res.json({
+    message: "Verification successful",
+  });
+};
+
+const resendVerifyEmail = async (req, res) => {
+  const { email } = req.body;
+  const { error } = schemas.email.validate({ email });
+  if (error) {
+    throw HttpError(400, error.message);
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(400, "missing required field email");
+  }
+  if (user.verify) {
+    throw HttpError(400, "Verification has already been passed");
+  }
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/${user.verificationToken}">Click verify email</a>`,
+  };
+  await sendEmail(verifyEmail);
+  res.json({
+    message: "Verification email sent",
   });
 };
 
@@ -33,6 +83,9 @@ const login = async (req, res) => {
   if (!user) {
     throw HttpError(401, "Invalid email or password");
   }
+  if(!user.verify) {
+    throw HttpError(401, "Email not verified");
+}
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw HttpError(401, "Invalid email or password");
@@ -115,4 +168,6 @@ module.exports = {
   logout: ctrlWrapper(logout),
   updateSubscription: ctrlWrapper(updateSubscription),
   updateAvatar: ctrlWrapper(updateAvatar),
+  verifyEmail: ctrlWrapper(verifyEmail),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
 };
--- models/user.js
+++ models/user.js
@@ -33,7 +33,15 @@ const userSchema = new Schema(
     avatarURL: {
       type: String,
       required: true,
-  }
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -46,20 +54,24 @@ const registerSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRedex).required(),
+})
+
 const loginSchema = Joi.object({
   email: Joi.string().pattern(emailRedex).required(),
   password: Joi.string().min(6).required(),
 });
 
 const updateSubscriptionSchema = Joi.object({
-  subscription: Joi.string().valid("starter", "pro", "business").required()
-})
-
+  subscription: Joi.string().valid("starter", "pro", "business").required(),
+});
 
 const schemas = {
   registerSchema,
+  emailSchema,
   loginSchema,
-  updateSubscriptionSchema
+  updateSubscriptionSchema,
 };
 
 const User = model("user", userSchema);
